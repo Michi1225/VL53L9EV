cc4134bb116919f88b9ecce5b0c57998c2a64d0e
Michael Haslinger <michi9394@gmail.com>
2026-08-21 11:30:29 +0200

    Ignore generated build outputs

--- FSBL/build/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "VL53L9_eval_FSBL.map"
-  )
-endif()
